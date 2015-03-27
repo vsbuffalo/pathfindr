0e14e5adba15693a978ab3094b8cd15dfe67bec1
Davide Risso <risso.davide@gmail.com>
2015-03-26 21:56:35 -0700

    Self-contained function to parse Illumina FASTQ files.
    Although it's probably not the way we want to do this, it is a
    minimal working example.
    
    Note that it assumes that sample_name has no underscores.

--- R/illumina.R
+++ R/illumina.R
@@ -10,3 +10,21 @@ extract_illumina <- function(paths) {
                                set_number=is.integer)
     validated <- validate_metadata(metadata, validate_functions)
 }
+
+illumina_self_contained <- function(paths) {
+    cols <- c("sample_name", "barcode_sequence", "lane", "read_number", "set_number")
+    paths <- basename(paths[grep("\\w+_L\\d{3}_R\\d_\\d{3}.fastq.gz", paths)])
+
+    parsed <- strsplit(paths, "_")
+    sample_name <- sapply(parsed, function(x) x[1])
+    read <- sapply(parsed, function(x) x[length(x)-1])
+    lane <- sapply(parsed, function(x) x[length(x)-2])
+    barcode <- sapply(parsed, function(x) paste(x[2:(length(x)-3)], collapse="_"))
+    set_num <- sapply(parsed, function(x) {
+        y <- x[length(x)]
+        return(strsplit(y, ".", fixed=TRUE)[[1]][1])
+    })
+    retval <- data.frame(sample_name, barcode, lane, read, set_num)
+    colnames(retval) <- cols
+    return(retval)
+}
